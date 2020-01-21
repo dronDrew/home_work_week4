ae8e1651663b43b8d4d7a9353f67ef71606b9c18
Андрій Дудчак <andrew.melauda@gmail.com>
2020-01-21 18:33:03 +0200

    final.version

--- home_work_week4/Printing_server.h
+++ home_work_week4/Printing_server.h
@@ -2,21 +2,23 @@
 #include "dependies.h"
 class Printing_server
 {
-	class Printing_docum {
-	public:
-		int client;
-		int priore;
-		int time;
-		Printing_docum* nextdocum;
-		Printing_docum* priviosdocum;
-		Printing_docum(int client, int priore, int time);
-	};
+    class Printing_docum {
+    public:
+        int client;
+        int priore;
+        int time;
+        Printing_docum* nextdocum;
+        Printing_docum* priviosdocum;
+        Printing_docum(int client, int priore, int time);
+    };
 public:
-	Printing_docum* docum;
-	Printing_docum* deleted_docum;
-	Printing_server();
-	void Add(int client, int priore, int time);
-	void Pop();
-	void Show_statictic();
+    Printing_docum* docum;
+    Printing_docum* deleted_docum;
+    Printing_server();
+    void Add(int client, int priore, int time);
+    void Add_tostory(int client, int priore, int time);
+    void Pop();
+    void Show_statictic();
+    void Show();
 };
 
